608e69d063d672e076732a205e2fea95a3f35d78
Marcus Holmgren <marcus.holmgren1@gmail.com>
2018-02-05 21:21:10 +0100

    Added Gatsby starter template for my user site.

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'this.noteToSelf',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+};
---
+++ src/pages/page-2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>Hi again!</h1>
+    <p>Sorry, no content yet.</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default SecondPage
